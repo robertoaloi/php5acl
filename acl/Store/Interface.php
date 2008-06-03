2f1802947f8aa2c515bc2db6cc7eac124df6103d
prof3ta <prof3ta@b4223135-bf4e-0410-9273-517f6a5e5af6>
2008-06-03 18:29:02 +0000

    php5acl Libraries

--- acl/Store/Interface.php
+++ acl/Store/Interface.php
@@ -4,10 +4,7 @@ interface  StoreInterface
 {
 
 	public function addRole(RoleInterface $role, $parent);
-	public function deleteAllRoles();
-	public function deleteAllResources();
-	public function deleteAllPermissions();
-	public function deleteAllRules();
+	public function deleteAllEntries($etryType);
 
 }
 
